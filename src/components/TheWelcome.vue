490bddf1dee78b356416a00b380e355f4d34f3ee
yiiqii <yfj5tzl2005@sina.com>
2024-04-09 10:42:09 +0800

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Player } from '@galacean/effects';
+import '@galacean/effects-plugin-model';
+
+const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*B_VAQafaBMAAAAAAAAAAAAAADlB4AQ';
+
+export default {
+  player: null,
+  mounted() {
+    const container = this.$refs.container;
+    this.player = new Player({
+      container,
+      interactive: true,
+    });
+
+    this.player.loadScene(json);
+  },
+  beforeUnmount() {
+    this.player?.dispose();
+  },
+}
+</script>
+
+<template>
+  <div ref="container" style="width: 100vw; height: 100vh;"></div>
+</template>
